e4f1b759b6f63116927003416910e0d910638c93
ggreena <ggreenaway29@gmail.com>
2022-12-30 19:55:50 -0500

    struggling

--- js/script.js
+++ js/script.js
@@ -19,38 +19,63 @@ This function will create and insert/append the elements needed to display a "pa
 Start Index = (page parameter * items per page) - items per page
 End Index = page parameter * items per page
 */
-function showPage (list, page) {
-   const startIndex = (page * perPage) - perPage
-   const endIndex = page * perPage
-   const perPage = 9;
-   const studentList = document.getElementsByClassName(student-list);
-   studentList.innerHTML = "";
-   for (let i =0; i<studentList.length; i++) {
-      if (i>=startIndex && i<endIndex) {
-         const body = document.querySelector('body');
+// function showPage (list, page) {
+//    const perPage = 9;
+//    const startIndex = (page * perPage) - perPage
+//    const endIndex = page * perPage
+   
+//    // how to make perpage more dynamic?
+//    const studentList = document.getElementsByClassName('student-list');
+//    studentList.innerHTML = "";
+//    for (let i =0; i<studentList.length; i++) {
+//       if (i>=startIndex && i<endIndex) {
+//          // const body = document.querySelector('body');
 
-         const lilist = document.createElement('li');
-         lilist.className = "student-item cf"
-         body.appendChild(lilist);
+//          const lilist = document.createElement('li');
+//          lilist.className = "student-item cf"
+//          studentList.appendChild(lilist);
+//          console.log(lilist);
+         
 
-         const div = document.querySelector('div')
-         div.className = "student-details";
+         // const div1 = document.querySelector('div')
+         // div1.className = "student-details";
+         // lilist.appendChild(div1);
 
-         const img = document.createElement('img')
-         img.className = "avatar";
-         img.src = ;
-         // reference the pic array on data.js? data[i]
-         img.alt = "Profile Picture"
-         div.appendChild(img);
+         // const img = document.createElement('img')
+         // img.className = "avatar";
+         // img.src = `${data[i].picture.medium}`;
+         // img.alt = "Profile Picture"
+         // div1.appendChild(img);
 
-         const name = document.createElement('h3');
-         name.innerHTML = `${first} ${last}`
-         //// reference the name array on data.js? data[i]
-         div.appendChild(name);
-      }
-   }
-}
+         // const name = document.createElement('h3');
+         // name.innerHTML = `${data[i].name.first} ${data[i].name.last}`
+         // div1.appendChild(name);
 
+         // const email = document.createElement('span');
+         // email.innerHTML = `${data[i].email}`
+         // email.className = "email";
+         // div1.appendChild(email);
+
+         // const div2 = document.querySelector('div')
+         // div1.className = "student-details";
+         // studentList.appendChild(div2);
+
+         // const joineddate = document.createElement('span');
+         // joineddate.innerHTML = `${data[i].registered.date}`
+//          // div2.appendChild(joineddate);
+//       }
+//    }
+   
+// }
+
+
+// console.log(showPage());
+const studentList = document.getElementsByClassName('student-list');
+studentList.innerHTML = "";
+const lilist = document.createElement('li');
+lilist.className = "student-item cf"
+studentList.insertAdjacentHTML(beforeend, 'lilist');
+console.log(lilist);
 
 /*
 Create the `addPagination` function
